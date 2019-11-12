04fd08c0d10face3b00d746b3fe4346413efb24c
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2019-11-12 12:39:54 +0200

    refactor: lexical_analysis <--> syntax_analysis

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 import file_operations
-import lexical_analysis
+import syntax_analysis
 import outputs
-from syntax_analysis import AuxiliarTables
+from lexical_analysis import AuxiliarTables
 from java_parser import JavaParser
 from java_grammar import CONTEXT_FREE_GRAMMAR
 from converters.pascal_converter import print_pascal_equivalent, JAVA_PASCAL_MAPPING
@@ -11,16 +11,16 @@ lines = file_operations.read_lines_from_file('java_files/Ex.java')
 if len(lines) == 0:
     exit()
 
-_syntax_analysis = AuxiliarTables()
-response = _syntax_analysis.execute(lines)
+_lexical_analysis = AuxiliarTables()
+response = _lexical_analysis.execute(lines)
 if response.get("code") == -1:
     print(response.get("message"))
 else:
     # print(response.get("lexemes"))
     # print(response.get("symbols"))
-    outputs.print_lexema_table(_syntax_analysis.get_lexeme_table())
+    outputs.print_lexema_table(_lexical_analysis.get_lexeme_table())
 
-    outputs.print_symbol_table(_syntax_analysis.get_symbol_table())
+    outputs.print_symbol_table(_lexical_analysis.get_symbol_table())
 
     # [x] Declaração de variáveis do tipo primitivo local
     # [X] Declaração de variáveis do tipo primitivo global
@@ -29,10 +29,10 @@ else:
     # [x] Funções com retorno
     # [ ] Estruturas de controlo (if e while)
 
-    parse_result = lexical_analysis.execute(_syntax_analysis.get_lexeme_table(), CONTEXT_FREE_GRAMMAR)
+    parse_result = syntax_analysis.execute(_lexical_analysis.get_lexeme_table(), CONTEXT_FREE_GRAMMAR)
 
     if parse_result:
         java_class = JavaParser().get_class_declaration(parse_result,
-                                                        _syntax_analysis.get_symbol_table().get_identifiers(),
+                                                        _lexical_analysis.get_symbol_table().get_identifiers(),
                                                         JAVA_PASCAL_MAPPING)
         print_pascal_equivalent(java_class)
